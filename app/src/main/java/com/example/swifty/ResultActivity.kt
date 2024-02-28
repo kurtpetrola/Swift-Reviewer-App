920e4c2c8c2fb01ec24b36a803c1c4912d19d0c7
Kurt Guen Petrola <kurtguenpetrola2.0@gmail.com>
2024-02-28 23:14:46 +0800

    gradle

--- app/src/main/java/com/example/swifty/ResultActivity.kt
+++ app/src/main/java/com/example/swifty/ResultActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+    
 class ResultActivity : AppCompatActivity() {
 
     lateinit var correctAns:TextView
